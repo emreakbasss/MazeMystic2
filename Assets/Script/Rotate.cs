ed90f1c48b40b72f27d833b5565e081efbb394f2
Emre Akbas <e.akbas@outlook.com.tr>
2024-03-16 23:06:12 +0300

    .newwww

--- Assets/Script/Rotate.cs
+++ Assets/Script/Rotate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenTrapDoor : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
